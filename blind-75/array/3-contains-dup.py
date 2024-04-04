ceb3906c6e15f8bfb679a864b5251ebe59f41fae
Amey Mehta <ameymehta@gmail.com>
2024-04-04 18:15:32 -0400

    More array problems solved

--- blind-75/array/3-contains-dup.py
+++ blind-75/array/3-contains-dup.py
@@ -1,2 +1,11 @@
 # https://leetcode.com/problems/contains-duplicate/description/
 
+class Solution(object):
+    def containsDuplicate(self, nums):
+        m = {}
+        for i in range(len(nums)):
+            if nums[i] in m:
+               return True 
+            else:
+                m[nums[i]] = i
+        return False
---
+++ blind-75/array/8-search-in-rotated-sorted-array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         return i
+        # return -1
+        l= 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) / 2
+            if nums[mid] == target:
+                return mid
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target and nums[mid] >= target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[r] >= target and nums[mid] <= target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
