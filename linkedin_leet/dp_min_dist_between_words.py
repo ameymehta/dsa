978a74032ba4134e4d86c49be51558f09dbd61e4
Amey Mehta <ameymehta@gmail.com>
2024-04-07 11:13:23 -0400

    Slight fix

--- linkedin_leet/dp_min_dist_between_words.py
+++ linkedin_leet/dp_min_dist_between_words.py
@@ -7,18 +7,20 @@ class Solution(object):
         """
         m = len(word1)
         n = len(word2)
-        memo = [[0 for i in range(n+1)] for j in range(m+1)]
-        
-        for i in range(1, m+1):
+        memo = [[0 for j in range(n + 1)] for i in range(m + 1)]
+
+        for i in range(1, m + 1):
             memo[i][0] = i
-        for j in range(1, n+1):
+        for j in range(1, n + 1):
             memo[0][j] = j
-            
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if word1[i-1] == word2[j-1]:
-                    memo[i][j] = memo[i-1][j-1]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    memo[i][j] = memo[i - 1][j - 1]
                 else:
-                    memo[i][j] = min(memo[i-1][j-1], memo[i][j-1], memo[i-1][j]) + 1
-            
+                    memo[i][j] = (
+                        min(memo[i - 1][j - 1], memo[i][j - 1], memo[i - 1][j]) + 1
+                    )
+
         return memo[m][n]
