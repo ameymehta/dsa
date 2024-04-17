589088da47c0a99ec3acfd5477d045da0952c1c4
Amey Mehta <ameymehta@gmail.com>
2024-04-16 22:20:33 -0400

    3 more DP, 5 more to go

---
+++ blind-75/dynamic-programming/6-num-of-combinations-to-a-sum.py
@@ -0,0 +1,33 @@
+# Example 1:
+# Input: nums = [1,2,3], target = 4
+# Output: 7
+# Explanation:
+# The possible combination ways are:
+# (1, 1, 1, 1)
+# (1, 1, 2)
+# (1, 2, 1)
+# (1, 3)
+# (2, 1, 1)
+# (2, 2)
+# (3, 1)
+# Note that different sequences are counted as different combinations.
+
+# Example 2:
+# Input: nums = [9], target = 3
+# Output: 0
+
+
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for i in range(1, target + 1):
+            for num in nums:
+                if num <= i:
+                    dp[i] += dp[i - num]
+        return dp[target]
