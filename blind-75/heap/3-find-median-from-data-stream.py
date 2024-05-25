cad8dcab9d247e8574d6fae9fe8700476dd18d77
Amey Mehta <ameymehta@gmail.com>
2024-05-25 16:54:24 -0400

    Removing underscores

--- blind-75/heap/3-find-median-from-data-stream.py
+++ blind-75/heap/3-find-median-from-data-stream.py
@@ -4,8 +4,8 @@ from heapq import *
 class MedianFinder(object):
 
     def __init__(self):
-        self.max_heap = []
-        self.min_heap = []
+        self.maxHeap = []
+        self.minHeap = []
 
     def addNum(self, num):
         """
@@ -13,24 +13,24 @@ class MedianFinder(object):
         :rtype: None
         """
         # add based on tops of each heap
-        if not self.max_heap or -self.max_heap[0] > num:
-            heappush(self.max_heap, -num)
+        if not self.maxHeap or -self.maxHeap[0] > num:
+            heappush(self.maxHeap, -num)
         else:
-            heappush(self.min_heap, num)
+            heappush(self.minHeap, num)
         # rebalance if needed
-        if len(self.max_heap) > len(self.min_heap) + 1:
-            heappush(self.min_heap, -heappop(self.max_heap))
-        elif len(self.min_heap) > len(self.max_heap):
-            heappush(self.max_heap, -heappop(self.min_heap))
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heappush(self.minHeap, -heappop(self.maxHeap))
+        elif len(self.minHeap) > len(self.maxHeap):
+            heappush(self.maxHeap, -heappop(self.minHeap))
 
     def findMedian(self):
         """
         :rtype: float
         """
-        if len(self.max_heap) == len(self.min_heap):
-            return -self.max_heap[0] / 2.0 + self.min_heap[0] / 2.0
+        if len(self.maxHeap) == len(self.minHeap):
+            return -self.maxHeap[0] / 2.0 + self.minHeap[0] / 2.0
         else:
-            return -self.max_heap[0] / 1.0
+            return -self.maxHeap[0] / 1.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
