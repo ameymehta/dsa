0a155000633c50247971b78a62c429ad144f9982
Amey Mehta <ameymehta@gmail.com>
2024-05-27 11:12:05 -0400

    Semantics

--- blind-75/string/4-valid-anagram.py
+++ blind-75/string/4-valid-anagram.py
@@ -17,17 +17,11 @@ class Solution(object):
         if len(s) != len(t):
             return False
         n = len(s)
-        freq = {}
+        m = defaultdict(int)
         for i in range(n):
-            if s[i] not in freq:
-                freq[s[i]] = 0
-            freq[s[i]] += 1
-            if t[i] not in freq:
-                freq[t[i]] = 0
-            freq[t[i]] -= 1
-
-        for k in freq:
-            if freq[k] != 0:
+            m[s[i]] += 1
+            m[t[i]] -= 1
+        for key in m:
+            if m[key] != 0:
                 return False
-
         return True
