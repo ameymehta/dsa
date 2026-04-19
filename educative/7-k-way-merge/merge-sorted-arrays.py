87a9d0d8ff2dd9f3f4f34c24c556e0fcce9f910b
Amey Mehta <ameymehta@Ameys-MacBook.local>
2026-04-19 10:59:22 -0400

    Removed non-working and duplicate code

--- educative/7-k-way-merge/merge-sorted-arrays.py
+++ educative/7-k-way-merge/merge-sorted-arrays.py
@@ -1,16 +1,17 @@
 def merge_sorted(nums1, m, nums2, n):
-    p1 = m - 1
-    p2 = n - 1
-    for p in range(m + n - 1, -1, -1):
-        if p2 < 0:
+    pt1 = m - 1
+    pt2 = n - 1
+    for i in range(m+n-1, -1, -1):
+        if pt2 < 0:
             break
-        if p1 >= 0 and nums1[p1] > nums2[p2]:
-            nums1[p] = nums1[p1]
-            p1 -= 1
+        if pt1 >= 0 and nums1[pt1] > nums2[pt2]:
+            nums1[i] =  nums1[pt1]
+            pt1 -= 1
         else:
-            nums1[p] = nums2[p2]
-            p2 -= 1
+            nums1[i] =  nums2[pt2]
+            pt2 -= 1
     return nums1
+    
 
 # Driver code
 def main():
