0a235ad640031a578b26183899a68f05857ab1d6
Amey Mehta <ameymehta@Ameys-MacBook.local>
2026-04-19 10:30:00 -0400

    Removed non-working and duplicate code

--- educative/1-two-pointers/remove_duplicates.py
+++ educative/1-two-pointers/remove_duplicates.py
@@ -1,19 +1,14 @@
 def remove_duplicates(arr):
-    next = 1
-    i = 1
-    while i < len(arr):
-        if arr[i-1] == arr[i]:
-            arr[next] = arr[i]
-            next += 1
-        i += 1
-    # return the next index as it is exactly
-    # the number of the unique elements we have in the array
-    return next
-
+  non_dup_tracker = 0
+  for i in range(1, len(arr)):
+    if arr[non_dup_tracker] != arr[i]:
+      arr[non_dup_tracker + 1] = arr[i]
+      non_dup_tracker += 1
+  return non_dup_tracker + 1
 
 def main():
-    print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
-    print(remove_duplicates([2, 2, 2, 11]))
+  print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+  print(remove_duplicates([2, 2, 2, 11]))
 
 
 main()
