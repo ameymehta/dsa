bb0a7689ed195885c12df38c59b5741f04d3ea3c
Amey Mehta <ameymehta@gmail.com>
2026-04-24 12:30:16 -0400

    Update smallest_subarray_with_given_sum logic
    Add condition to check if window sum equals target sum.

--- educative/3-sliding-window/smallest_subarray_with_given_sum.py
+++ educative/3-sliding-window/smallest_subarray_with_given_sum.py
@@ -8,7 +8,8 @@ def smallest_subarray_with_given_sum(s, arr):
     for window_end in range(len(arr)):
         window_sum += arr[window_end]
         while window_sum >= s:
-            min_length = min(min_length, window_end-window_start+1)
+            if window_sum == s:
+                min_length = min(min_length, window_end-window_start+1)
             window_sum -= arr[window_start]
             window_start += 1
     if min_length == math.inf:
