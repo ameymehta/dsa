36aea4cac129d205da7d79b43dae8ca07c7832d6
Amey Mehta <ameymehta@Ameys-MacBook.local>
2026-04-25 13:44:55 -0400

    heapq implementation needs a tie-breaker

--- blind-75/heap/1-merge-k-sorted-lists-with-heap.py
+++ blind-75/heap/1-merge-k-sorted-lists-with-heap.py
@@ -27,3 +27,22 @@ class Solution(object):
             if node.next:
                 heapq.heappush(h, (node.next.val, node.next))
         return dummy.next
+    
+    # added count as tie breaker when heapq implementation compares node.val and if there is a tie
+    def mergeKLists__2026(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        h = []
+        count = 0
+        for node in lists:
+            if node:
+                heapq.heappush(h, (node.val, count, node))
+                count += 1
+        dummy = ListNode()
+        cur = dummy
+        while h:
+            val, cnt, node = heapq.heappop(h)
+            cur.next = node
+            cur = cur.next
+            if node.next:
+                heapq.heappush(h, (node.next.val, count, node.next))
+                count += 1
+        return dummy.next
