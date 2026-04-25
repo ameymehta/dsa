978532199ceb170718c154761c0ecd7f76420696
Amey Mehta <ameymehta@Ameys-MacBook.local>
2026-04-25 09:56:51 -0400

    New approach to validate parentheses

--- blind-75/string/6-valid-parenthesis.py
+++ blind-75/string/6-valid-parenthesis.py
@@ -23,3 +23,24 @@ class Solution(object):
         if len(stack) == 0:
             return True
         return False
+    
+    # using map to remember pairs
+    def is_valid_parentheses(self, s):
+        stack = []
+        pairs = { '(' : ')', '{' : '}', '[' : ']'}
+        for ch in s:
+            if ch in pairs:
+                stack.append(ch)
+            elif ch in pairs.values():
+                if not stack or pairs[stack.pop()] != ch:
+                    return False
+            # ignore other chars
+
+        # stack should be empty at the end of for loop for s to be valid
+        return len(stack) == 0
+
+    def test_cases(self):
+        print(self.is_valid_parentheses("()[]{}"))   # True
+        print(self.is_valid_parentheses("([)]"))     # False
+        print(self.is_valid_parentheses("{[]}"))     # True
+        print(self.is_valid_parentheses("("))        # False
