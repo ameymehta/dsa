0edd7e98c3c7327407770486e503738d54ccf0fa
Amey Mehta <ameymehta@Ameys-MacBook.local>
2026-05-05 11:27:43 -0400

    alternate approach for max subarray prod

--- blind-75/array/6-max-subarray-product.py
+++ blind-75/array/6-max-subarray-product.py
@@ -12,3 +12,12 @@ class Solution(object):
             if memo[i] > maxProduct:
                 maxProduct = memo[i]
         return maxProduct
+    
+    def maxProduct2(self, nums):
+        max_prod = nums[0]
+        prev_prod = nums[0]
+        for i in range(1, len(nums)):
+            cur_prod = max(prev_prod * nums[i], nums[i])
+            prev_prod = cur_prod
+            max_prod = max(cur_prod, max_prod)
+        return max_prod
